68f249eee2e9caa02723dfb642237d04590fdfa5
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-28 18:16:29 -0700

    implement show action and view for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    post = Post.find_by(id: params[:id])
+    @post = Post.find_by(id: params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/', to: 'posts#index'
   get '/posts/new', to: 'posts#new'
   post '/posts', to: 'posts#create'
+  get '/posts/:id', to: 'posts#show'
 
 end
